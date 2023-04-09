f88912f32a87ab46a217d8d4c198e9665623f70d
Haoran <weng1043282095@gmail.com>
2023-04-08 23:58:11 -0400

    more centered button for front screen

--- frontend/src/components/FrontPageScreen.tsx
+++ frontend/src/components/FrontPageScreen.tsx
@@ -4,7 +4,7 @@ export const FrontPageScreen = () => {
     return(
         <div className="bg-[url('/img/welcome.png')] bg-center bg-cover relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p- lg:max-w-xl">
-                <form>
+                <div className="absolute w-screen right-80 top-80">
                     <div className="mt-6">
                         <Link to="/login">
                             <input type="submit" value="Login" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
@@ -18,7 +18,7 @@ export const FrontPageScreen = () => {
                             <input type="submit" value="Continue as Guest" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
                         </Link>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     )
