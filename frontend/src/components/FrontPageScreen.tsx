8dd4149ee8143ac31cdcd9b600ac4a6f4bc35fe8
Haoran Weng <weng1043282095@gmail.com>
2023-04-09 19:37:23 -0400

    new front screen v2

--- frontend/src/components/FrontPageScreen.tsx
+++ frontend/src/components/FrontPageScreen.tsx
@@ -10,21 +10,21 @@ export const FrontPageScreen = () => {
                     className="ml-16 mt-10 mb-10"
                     src={mapsonLogo}
                 ></img>
-                <span className="text-white text-4xl mb-10">
+                <span className="text-white text-4xl">
                 Your go-to tool for map interactivity
                 </span>
 
-                <div className="mt-10">
+                <div className="mt-16 text-3xl">
                     <Link to="/login">
-                        <input type="submit" value="Login" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                        <input type="submit" value="Login" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" />
                     </Link>
                     <Link to="/register">
-                        <input type="submit" value="Register" className="bg-blue ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                        <input type="submit" value="Register" className="bg-blue ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" />
                     </Link>
                 </div>
-                <div className="mt-6">
+                <div className="mt-6 text-3xl">
                     <Link to="/discover">
-                        <input type="submit" value="Continue as Guest" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                        <input type="submit" value="Continue as Guest" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" />
                     </Link>
                 </div>
 
