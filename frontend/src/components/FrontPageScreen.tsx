11bcde26b92966cdfbcc0e0634cfd57005683084
Haoran Weng <weng1043282095@gmail.com>
2023-04-09 19:31:31 -0400

    new front screen

--- frontend/src/components/FrontPageScreen.tsx
+++ frontend/src/components/FrontPageScreen.tsx
@@ -1,24 +1,33 @@
 import { Link } from "react-router-dom"
+import mapsonLogo from "../assets/MapSON-logo-outlined copy.png";
 
 export const FrontPageScreen = () => {
-    return(
-        <div className="bg-[url('/img/welcome.png')] bg-center bg-cover relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p- lg:max-w-xl">
-                <div className="absolute w-screen right-80 top-80">
-                    <div className="mt-6">
-                        <Link to="/login">
-                            <input type="submit" value="Login" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
-                        </Link>
-                        <Link to="/register">
-                            <input type="submit" value="Register" className="bg-blue ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
-                        </Link>
-                    </div>
-                    <div className="mt-6">
-                        <Link to="/discover">
-                            <input type="submit" value="Continue as Guest" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
-                        </Link>
-                    </div>
+    return (
+        <div className="bg-[url('/img/welcome2.png')] bg-center bg-cover relative flex flex-col justify-center h-screen overflow-hidden">
+            <div className="w-1/2 p-10 h-screen">
+
+                <img
+                    className="ml-16 mt-10 mb-10"
+                    src={mapsonLogo}
+                ></img>
+                <span className="text-white text-4xl mb-10">
+                Your go-to tool for map interactivity
+                </span>
+
+                <div className="mt-10">
+                    <Link to="/login">
+                        <input type="submit" value="Login" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                    </Link>
+                    <Link to="/register">
+                        <input type="submit" value="Register" className="bg-blue ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                    </Link>
                 </div>
+                <div className="mt-6">
+                    <Link to="/discover">
+                        <input type="submit" value="Continue as Guest" className="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+                    </Link>
+                </div>
+
             </div>
         </div>
     )
