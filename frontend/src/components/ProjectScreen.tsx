22af31d9d820a4163290e596cd4940a50d09346e
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2023-04-10 15:44:56 -0400

    Fix merge artifact

--- frontend/src/components/ProjectScreen.tsx
+++ frontend/src/components/ProjectScreen.tsx
@@ -56,7 +56,7 @@ export const ProjectScreen = () => {
   const canEdit = true; // allow editing for build 2
 
   return (
-    <div className="bg-black w-screen h-full overflow-hidden">
+    <div className="bg-black w-screen h-[calc(100vh-64px)]">
       <ProjectNavbar
         shareOpen={shareOpen}
         setShareOpen={setShareOpen}
@@ -66,7 +66,7 @@ export const ProjectScreen = () => {
         sidePanelToggle={sidePanelToggle}
       />
 
-      <div className="flex flex-row h-[calc(100vh-64px)]">
+      <div className="flex flex-row">
         <MapComponent
           canEdit={canEdit}
           setSelectedFeature={setSelectedFeature}
