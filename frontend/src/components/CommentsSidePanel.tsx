ee6655916306d51143930a4bfbfd2d5266943ba6
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2023-04-10 17:06:09 -0400

    Updated styles (#37)
    * Updated styles
    
    * Only allow input if user is logged in

--- frontend/src/components/CommentsSidePanel.tsx
+++ frontend/src/components/CommentsSidePanel.tsx
@@ -1,34 +1,46 @@
 import React from "react";
 import CommentCard from "./CommentCard";
 
+import { RootState } from "../models";
+import { useSelector } from "react-redux";
+
 const CommentsSidePanel = () => {
+  const user = useSelector((state: RootState) => state.user.currentUser);
+
+  const canComment = !!user;
+
   return (
     <div
-      className="bg-gray p-2 z-0 text-white h-[calc(100vh-64px)]"
+      className="bg-gray z-0 text-white h-[calc(100vh-64px)]"
       style={{ minWidth: "20vw" }}
     >
-      <div className="grid grid-cols-1 gap-3 relative p-2">
-        <div>
-          <CommentCard />
-        </div>
-        <div>
-          <CommentCard />
-        </div>
-        <div>
-          <CommentCard />
-        </div>
-        <div>
-          <CommentCard />
-        </div>
+      <div className="overflow-y-scroll h-[92%]">
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
       </div>
-      <div className="mt-40">
-        <input
-          className="peer h-full w-55 rounded-[5px] border border-blue-gray-200 border-t-transparent bg-white px-3 py-2 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-          placeholder="Leave a comment..."
-        />
-        <button className="rounded-md ml-1 pr-5 pl-5 pt-0.5 pb-0.5 h-8 bg-blue">
-          Comment
-        </button>
+
+      <div className="w-full h-[calc(8%-64px)]">
+        <div className="flex flex-row mx-4 my-4">
+          <input
+            className="peer w-[75%] rounded-[5px] border border-blue-gray-200 border-t-transparent bg-white px-3 py-2 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 disabled:opacity-75"
+            placeholder={
+              canComment ? "Leave a comment..." : "Please log in to comment"
+            }
+            disabled={!canComment}
+          />
+          <button
+            className="w-[25%] rounded-md ml-1 px-1 py-1 bg-blue disabled:opacity-75"
+            disabled={!canComment}
+          >
+            Comment
+          </button>
+        </div>
       </div>
     </div>
   );
