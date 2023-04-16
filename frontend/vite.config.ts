8868fe3122ea80f81200a469ebb983022722624f
Haoran <weng1043282095@gmail.com>
2023-04-16 17:14:16 -0400

    setup import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -7,8 +8,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [react()],
-
+    plugins: [
+      react(),
+      nodePolyfills({
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true,
+      }),
+    ],
+    define: { global: 'window' },
     server: {
       proxy: {
         '^/api': {
