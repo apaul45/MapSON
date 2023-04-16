d845c6a84fdcbb6b24c575a972edd4fa13577003
Ayon Paul <ayon.paul@stonybrook.edu>
2023-04-15 23:13:18 -0400

    Fix and update cypress tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'cypress'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env,
+      }
+      return config
     },
   },
 })
--- frontend/cypress/e2e/account.cy.ts
+++ frontend/cypress/e2e/account.cy.ts
@@ -1,13 +1,17 @@
+const username = Cypress.env('CYPRESS_USER')
+const password = Cypress.env('CYPRESS_PASSWORD')
+
 describe('Login Screen Test', () => {
   beforeEach(() => cy.visit('http://127.0.0.1:5173/login'))
 
   it('should login user and go to home page', () => {
-    cy.get('#username').type('user')
-    cy.get('#password').type('password')
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
 
     cy.contains('Login').click()
 
     cy.location('pathname').should((path) => expect(path).to.include('/home'))
+    cy.get('#user-menu-button').should('exist').click()
   })
 
   it('should go to forget password page', () => {
@@ -22,8 +26,8 @@ describe('Logout Test', () => {
   beforeEach(() => cy.visit('http://127.0.0.1:5173/login'))
 
   it('should login and logout user', () => {
-    cy.get('#username').type('user')
-    cy.get('#password').type('password')
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
 
     cy.contains('Login').click()
 
