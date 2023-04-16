94e00431ba7d52aa7cd28edff5fd73884a16e477
Ayon Paul <ayon.paul@stonybrook.edu>
2023-04-15 22:25:19 -0400

    Change output directory back to default

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,10 +8,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    build: {
-      outDir: './wwwroot/app/',
-      sourcemap: true,
-    },
 
     server: {
       proxy: {
