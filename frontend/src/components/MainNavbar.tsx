06c5bf72b3d88eea056d5bba2f9221d32854f154
Ayon Paul <ayon.paul@stonybrook.edu>
2023-04-17 21:04:50 -0400

    Cleanup map & user store logic, remove create map boxes in home page

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import mapsonLogo from '/img/MapSON-logo-outlined copy.png'
-import { Link, useLocation } from 'react-router-dom'
-import { RootState, store } from '../models'
-import AccountCircle from './AccountCircle'
-import { useSelector } from 'react-redux'
-import { Menu, MenuHandler, MenuItem, MenuList } from '@material-tailwind/react'
+import { useState } from 'react';
+import mapsonLogo from '/img/MapSON-logo-outlined copy.png';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { RootState, store } from '../models';
+import AccountCircle from './AccountCircle';
+import { useSelector } from 'react-redux';
+import { Menu, MenuHandler, MenuItem, MenuList } from '@material-tailwind/react';
 
 export const MainNavbar = () => {
-  const location = useLocation()
-  const user = useSelector((state: RootState) => state.user.currentUser)
+  const location = useLocation();
+  const isSelected = (path: string) => (location.pathname === path ? 'selected-nav-btn' : 'nav-btn');
 
-  const isSelected = (path: string) =>
-    location.pathname === path ? 'selected-nav-btn' : 'nav-btn'
+  const user = useSelector((state: RootState) => state.user.currentUser);
+  const { mapStore } = store.dispatch;
+
+  const navigate = useNavigate();
+
+  const openAddDialog = () => mapStore.setAddDialog(true);
+
+  const handleCreateMap = async () => {
+    const id = await mapStore.createNewMap({
+      mapName: 'My Map',
+    });
+
+    if (id) navigate(`/project/${id}`);
+  };
 
   //Render the main navbar everywhere except any project related screens
   return (
@@ -24,10 +36,7 @@ export const MainNavbar = () => {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <Link to="/">
-                    <img
-                      className="hidden h-8 w-auto lg:block"
-                      src={mapsonLogo}
-                    ></img>
+                    <img className="hidden h-8 w-auto lg:block" src={mapsonLogo}></img>
                   </Link>
                 </div>
 
@@ -67,43 +76,22 @@ export const MainNavbar = () => {
                   /* <!-- Profile dropdown --> */
                   user ? (
                     <>
+                      {/* Add Map Button & Menu */}
                       <Menu placement="bottom-start">
                         <MenuHandler>
                           <button className="mr-2">
-                            <svg
-                              className="w-6 h-6 inline"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 4.5v15m7.5-7.5h-15"
-                              />
+                            <svg className="w-6 h-6 inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
-                            <svg
-                              className="w-4 h-4 inline"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="#FFFFFF"
-                              viewBox="0 0 320 512"
-                            >
+                            <svg className="w-4 h-4 inline" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" viewBox="0 0 320 512">
                               <path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z" />
                             </svg>
                           </button>
                         </MenuHandler>
                         <MenuList className="bg-gray text-white p-0 font-sans text-base">
-                          <MenuItem
-                            onClick={() =>
-                              store.dispatch.mapStore.setAddDialog(true)
-                            }
-                          >
-                            Import from Shapefile/GeoJSON
-                          </MenuItem>
+                          <MenuItem onClick={() => openAddDialog()}>Import from Shapefile/GeoJSON</MenuItem>
                           <hr className="align-middle" />
-                          <MenuItem>Create new Map</MenuItem>
+                          <MenuItem onClick={() => handleCreateMap()}>Create new Map</MenuItem>
                         </MenuList>
                       </Menu>
                       <AccountCircle />
@@ -111,21 +99,13 @@ export const MainNavbar = () => {
                   ) : (
                     <>
                       {location.pathname !== '/login' && (
-                        <Link
-                          to="/login"
-                          id="login-button"
-                          className="acct-btn mr-2"
-                        >
+                        <Link to="/login" id="login-button" className="acct-btn mr-2">
                           {' '}
                           Login{' '}
                         </Link>
                       )}
                       {location.pathname !== '/register' && (
-                        <Link
-                          to="/register"
-                          id="register-button"
-                          className="acct-btn"
-                        >
+                        <Link to="/register" id="register-button" className="acct-btn">
                           {' '}
                           Register{' '}
                         </Link>
@@ -139,5 +119,5 @@ export const MainNavbar = () => {
         </nav>
       )}
     </>
-  )
-}
+  );
+};
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,64 +1,63 @@
-import { Feature, FeatureCollection, Geometry } from 'geojson'
+import { Feature, FeatureCollection, Geometry } from 'geojson';
 
 export interface User {
-  email?: string
-  username: string
-  password: string
-  maps?: Map[]
+  email?: string;
+  username: string;
+  password: string;
+  maps?: Map[];
 }
 
 export interface Published {
-  isPublished: boolean
-  publishedDate?: Date
+  isPublished: boolean;
+  publishedDate?: Date;
 }
 
 export interface Comment {
-  username: string
-  comment: string
+  username: string;
+  comment: string;
 }
 
 // add mongodb _id type to geometries and features from `geojson` types
 export interface CommonGeoJSONData {
-  _id: string
+  _id: string;
 }
-export type GeometryExt = CommonGeoJSONData & Geometry
-export type FeatureExt = CommonGeoJSONData & Feature<GeometryExt>
-export type Features = Array<FeatureExt>
+export type GeometryExt = CommonGeoJSONData & Geometry;
+export type FeatureExt = CommonGeoJSONData & Feature<GeometryExt>;
+export type Features = Array<FeatureExt>;
 
-export type LGeoJsonExt = L.GeoJSON & CommonGeoJSONData & LayerExt
+export type LGeoJsonExt = L.GeoJSON & CommonGeoJSONData & LayerExt;
 
 export interface LayerExt {
-  clearCustomEventHandlers: Function | undefined
+  clearCustomEventHandlers: Function | undefined;
 }
 
 export interface Map {
-  _id: string
-  name: string
-  username: string
-  upvotes: User[]
-  downvotes: User[]
-  forks: number
-  downloads: number
-  published: Published
-  comments: Comment[]
-  features: Features
-  description?: string
-  updatedAt?: string
+  _id: string;
+  name: string;
+  username: string;
+  upvotes: User[];
+  downvotes: User[];
+  forks: number;
+  downloads: number;
+  published: Published;
+  comments: Comment[];
+  features: Features;
+  description?: string;
+  updatedAt?: string;
 }
 
 export interface Store {
-  currentMap: Map | null
-  maps: Map[]
-  userMaps: Map[]
-  deleteDialog: boolean
-  shareDialog: boolean
-  addDialog: boolean
+  currentMap: Map | null;
+  maps: Map[];
+  deleteDialog: boolean;
+  shareDialog: boolean;
+  addDialog: boolean;
 }
 
 export interface UserModel {
-  currentUser: User | null
+  currentUser: User | null;
 }
 
 export interface ErrorModel {
-  errorMessage: string | null
+  errorMessage: string | null;
 }
