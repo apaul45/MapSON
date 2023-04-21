b47e3e8b2331b8df8259ba2bf722015af07c3328
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2023-04-20 21:51:06 -0400

    Update project screen tests

--- frontend/cypress/e2e/project.cy.ts
+++ frontend/cypress/e2e/project.cy.ts
@@ -1,53 +1,63 @@
-import { login, logout, upload } from './account'
+import { login, logout, upload } from './account';
 
 describe('Project Screen Tests', () => {
-  beforeEach(() => cy.visit('http://127.0.0.1:5173/project/default'))
+  before(() => {
+    login();
+    cy.get('#plus-sign').parent().should('be.visible').click();
+    cy.contains('Create new Map').should('be.visible').click();
+    cy.location('href').should((path) => {
+      expect(path).to.include('/project');
+    });
+  });
+
+  beforeEach(() => {
+    login();
+    cy.get('.mapcard').last().click();
+  });
 
   it('should update the map name', () => {
-    cy.get('#project-namefield').should('not.exist')
-    cy.get('#project-name').dblclick()
+    cy.get('#project-namefield').should('not.exist');
+    cy.get('#project-name').dblclick();
 
-    cy.get('#project-namefield').clear().type('Cypress Map{enter}')
+    cy.get('#project-namefield').clear().type('Cypress Map{enter}');
 
-    cy.get('#project-namefield').should('not.exist')
-    cy.get('#project-name').should('contain.text', 'Cypress Map')
-  })
+    cy.get('#project-namefield').should('not.exist');
+    cy.get('#project-name').should('contain.text', 'Cypress Map');
+  });
 
   it('should exit the project', () => {
-    cy.get('#menu-button').click()
+    cy.get('#menu-button').click();
 
-    cy.contains('Exit project').click()
+    cy.contains('Exit project').click();
 
-    cy.location('pathname').should((path) =>
-      expect(path).to.include('/discover')
-    )
-  })
-})
+    cy.location('pathname').should((path) => expect(path).to.include('/discover'));
+  });
+});
 
 describe('Project Nav Bar Tests', () => {
   beforeEach(() => {
-    login()
-    upload()
-  })
-  afterEach(() => logout())
+    login();
+    upload();
+  });
+  afterEach(() => logout());
 
   it('should download a geojson', () => {
-    cy.get('#menu-button').click()
+    cy.get('#menu-button').click();
 
-    cy.contains('Download as').invoke('show').click({ force: true }).click()
+    cy.contains('Download as').invoke('show').click({ force: true }).click();
 
-    cy.contains('GeoJSON').should('exist').click()
-    cy.readFile('cypress/downloads/test.geo.json').should('exist')
-  })
+    cy.contains('GeoJSON').should('exist').click();
+    cy.readFile('cypress/downloads/test.geo.json').should('exist');
+  });
 
   it('should download a shapefile zip', () => {
-    cy.get('#menu-button').click()
+    cy.get('#menu-button').click();
 
-    cy.contains('Download as').invoke('show').click({ force: true }).click()
+    cy.contains('Download as').invoke('show').click({ force: true }).click();
 
-    cy.contains('Shapefile').should('exist').click()
-    cy.readFile('cypress/downloads/test.geo.json').should('exist')
-  })
-})
+    cy.contains('Shapefile').should('exist').click();
+    cy.readFile('cypress/downloads/test.geo.json').should('exist');
+  });
+});
 
-export {}
+export {};
