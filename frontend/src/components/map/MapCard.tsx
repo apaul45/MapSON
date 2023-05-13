20cc321214792a9ad3dda2a9f7e606a9013124e1
Ayon Paul <ayon.paul@stonybrook.edu>
2023-05-13 12:29:49 -0400

    Add published and share icons for mapcards

--- frontend/src/components/map/MapCard.tsx
+++ frontend/src/components/map/MapCard.tsx
@@ -14,6 +14,7 @@ interface IMapCardProps {
   date: string;
   preview: string;
   published: boolean;
+  userAccess: number; // Length of user access array
 }
 
 export default function MapCard({
@@ -27,6 +28,7 @@ export default function MapCard({
   date,
   preview,
   published,
+  userAccess,
 }: IMapCardProps) {
   useEffect(() => {
     if (upvoteIndex >= 0 && upvoteClass === '') {
@@ -176,51 +178,60 @@ export default function MapCard({
   };
 
   return (
-    <div
-      className={
-        published
-          ? 'max-w-sm rounded overflow-hidden shadow-lg relative bg-green-200'
-          : 'max-w-sm rounded overflow-hidden shadow-lg relative bg-white'
-      }
-    >
+    <div className="max-w-sm rounded overflow-hidden shadow-lg relative bg-white">
       <div
         className="relative hover:cursor-pointer mapcard"
         onClick={(e) => {
           goToProject(e);
         }}
       >
-        {location.pathname.includes('home') ? (
-          <button
-            onClick={(e) => {
-              deleteCard(e);
-              //handleSetCurrentMap();
-            }}
-            className="absolute top-0 right-0 "
-            id="delete-button"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
+        {
+          //Render delete button if this is a user's map
+          location.pathname.includes('home') ? (
+            <button
+              onClick={(e) => {
+                deleteCard(e);
+                //handleSetCurrentMap();
+              }}
+              className="absolute top-0 right-0 "
+              id="delete-button"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
-          </button>
-        ) : (
-          ''
-        )}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+          ) : (
+            ''
+          )
+        }
         <img className="w-full h-72" src={preview ? preview : '/img/defaultpreviewimg.png'} />
       </div>
 
       <div className="py-3 px-3">
-        <div className="font-bold text-xl text-left">{name}</div>
+        <div className="font-bold text-xl text-left">
+          {name}
+          <img
+            className="float-right w-8 h-8 outline-none"
+            src={
+              published
+                ? '/img/icons/publishedicon.png'
+                : userAccess >= 1
+                ? '/img/icons/shared-icon.png'
+                : '/img/white-backgrnd.png'
+            }
+          />
+        </div>
         {expand ? (
           !isEditDescriptionActive ? (
             <p
