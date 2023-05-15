f8e2cf297b3392c5055626e23f0f8e6e7f9f26e6
Ayon Paul <ayon.paul@stonybrook.edu>
2023-05-15 00:26:08 -0400

    Add Https code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,30 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 // @ts-ignore
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
+  if (mode.includes('dev')) {
+    return {
+      plugins: [
+        react(),
+        nodePolyfills({
+          // Whether to polyfill `node:` protocol imports.
+          protocolImports: true,
+        }),
+      ],
+    };
+  }
   return {
+    server: {
+      https: {
+        key: process.env.key,
+        cert: process.env.certificate,
+      },
+    },
     plugins: [
       react(),
       nodePolyfills({
@@ -14,5 +32,5 @@ export default defineConfig(({ mode }) => {
         protocolImports: true,
       }),
     ],
-  }
-})
+  };
+});
